75165419ce7f47efd6a3239649797aa45ad5c948
YashNarK <narenkrithick@gmail.com>
2024-01-10 21:46:02 +0530

    double circle border over logo added

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Circle, HStack, Image } from "@chakra-ui/react";
+import { Circle, HStack, Image, useColorModeValue } from "@chakra-ui/react";
 import ToggleModeButton from "../ToggleModeButton";
 import logo from "../../assets/logo.jpg";
 interface Props {
@@ -6,11 +6,15 @@ interface Props {
 }
 
 const NavBar = ({ className = "" }: Props) => {
+  const logoBorderColor = useColorModeValue("gray.800", "white");
+  const logoBorderColorReverse = useColorModeValue("white","gray.800");
   return (
     <div className={className}>
       <HStack justifyContent={"space-between"} px={5} py={3}>
-        <Circle size="65px" bg="tomato" bgGradient="linear(to-t,gray.800 50%,white 50%)">
-          <Image src={logo} borderRadius={"30px"} boxSize={"60px"}></Image>
+        <Circle size="60px" bg="tomato" bgColor={logoBorderColor}>
+          <Circle size="55px" bg="tomato" bgColor={logoBorderColorReverse}>
+            <Image src={logo} borderRadius={"30px"} boxSize={"50px"}></Image>
+          </Circle>
         </Circle>
         <ToggleModeButton />
       </HStack>

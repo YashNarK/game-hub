f973bb7414dbddc64c7d5167d4b9aa0ca637f098
YashNarK <narenkrithick@gmail.com>
2024-01-10 20:22:44 +0530

    Responsive grid added in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import ToggleModeButton from "./components/ToggleModeButton";
-
 
 function App() {
   return (
-    <ToggleModeButton />
+    <>
+      <ToggleModeButton />
+      <Grid
+        templateAreas={{
+          base: `"nav nav" "menu menu"`,
+          md: `"nav nav" "aside menu"`,
+        }}
+      >
+        <GridItem area={"nav"} bg="coral">
+          NAV
+        </GridItem>
+        <Show above="md">
+          <GridItem area={"aside"} bg="gold">
+            ASIDE
+          </GridItem>
+        </Show>
+        <GridItem area={"menu"} bg="red">
+          MENU
+        </GridItem>
+      </Grid>
+    </>
   );
 }
 export default App;

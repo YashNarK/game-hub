6b58b898f88ef6f02c822ec4ad9c3f168e8f3b0b
YashNarK <narenkrithick@gmail.com>
2024-01-11 00:36:36 +0530

    created custom hook useGames for fetching all the games

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
-import { Grid, GridItem, Show, useColorModeValue } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 
-
-
 function App() {
-
-  const navColor = useColorModeValue('','');
-  const asideColor = useColorModeValue('grey','gold');
-  const menuColor = useColorModeValue('blue','green');
   return (
     <>
-      
       <Grid
         templateAreas={{
           base: `"nav nav" "menu menu"`,
           md: `"nav nav" "aside menu"`,
         }}
       >
-        <GridItem area={"nav"} bg={navColor}>
-          <NavBar /> 
+        <GridItem area={"nav"}>
+          <NavBar />
         </GridItem>
         <Show above="md">
-          <GridItem area={"aside"} bg={asideColor}>
-            ASIDE
-          </GridItem>
+          <GridItem area={"aside"}>ASIDE</GridItem>
         </Show>
-        <GridItem area={"menu"} bg={menuColor}>
-          MENU
-        </GridItem>
+        <GridItem area={"menu"}>MENU</GridItem>
       </Grid>
     </>
   );
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import gameService, { GameData } from "./../services/game-service";
+import { AxiosError, CanceledError } from "../services/api-client";
+
+const useGames = () => {
+  const [games, setGames] = useState<GameData[]>([]);
+  const [httpErrors, setHttpErrors] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    let controllerAbort: () => void;
+
+    async function fetchGames(): Promise<void> {
+      try {
+        setIsLoading(true);
+        const { resp, cancel } = await gameService.getAll<GameData>();
+        controllerAbort = cancel;
+        setGames(resp.data);
+        setHttpErrors("");
+      } catch (error) {
+        if (error instanceof CanceledError) return;
+        const err = error as AxiosError;
+        setHttpErrors(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchGames();
+
+    return () => controllerAbort && controllerAbort();
+  }, []);
+
+  return {
+    games,
+    httpErrors,
+    isLoading,
+    setGames,
+    setHttpErrors,
+    setIsLoading,
+  };
+};
+
+export default useGames;

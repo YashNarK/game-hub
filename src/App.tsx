3f44594a841fb891e0faaccb4b56aafecfd29531
YashNarK <narenkrithick@gmail.com>
2024-01-10 18:25:01 +0530

    Toggle Button added to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, useColorMode } from "@chakra-ui/react";
+import { BsMoonStarsFill } from "react-icons/bs";
+import { ImSun } from "react-icons/im";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <Button colorScheme="white" onClick={toggleColorMode}>
+        {colorMode === "light" ? <BsMoonStarsFill color="black" /> : <ImSun color="yellow" />}
+      </Button>
+  );
 }
-
-export default App
+export default App;

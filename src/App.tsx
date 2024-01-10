e0817531754c78c74a18d023fb116a7b0997a45b
YashNarK <narenkrithick@gmail.com>
2024-01-10 20:32:58 +0530

    Added color toggles for responsice grid in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Grid, GridItem, Show, useColorModeValue } from "@chakra-ui/react";
 import ToggleModeButton from "./components/ToggleModeButton";
 
 function App() {
+
+  const navColor = useColorModeValue('coral','teal');
+  const asideColor = useColorModeValue('grey','gold');
+  const menuColor = useColorModeValue('blue','green');
   return (
     <>
       <ToggleModeButton />
@@ -11,15 +15,15 @@ function App() {
           md: `"nav nav" "aside menu"`,
         }}
       >
-        <GridItem area={"nav"} bg="coral">
+        <GridItem area={"nav"} bg={navColor}>
           NAV
         </GridItem>
         <Show above="md">
-          <GridItem area={"aside"} bg="gold">
+          <GridItem area={"aside"} bg={asideColor}>
             ASIDE
           </GridItem>
         </Show>
-        <GridItem area={"menu"} bg="red">
+        <GridItem area={"menu"} bg={menuColor}>
           MENU
         </GridItem>
       </Grid>

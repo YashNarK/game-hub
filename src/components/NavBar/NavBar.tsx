757bd87b2afd843d353e4f893bf6e0d700d16359
YashNarK <narenkrithick@gmail.com>
2024-01-10 21:37:56 +0530

    removed app.css contents. logo surrounded by colors effect added

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,17 +1,21 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { Circle, HStack, Image } from "@chakra-ui/react";
 import ToggleModeButton from "../ToggleModeButton";
-import logo from '../../assets/logo.jpg'
+import logo from "../../assets/logo.jpg";
 interface Props {
   className?: string;
 }
 
-const NavBar = ({className=""}:Props) => {
-  return <div className={className}>
-    <HStack justifyContent={"space-between"} px={5} py={3}>
-        <Image src={logo}  borderRadius={"30px"} boxSize={'60px'}></Image>
+const NavBar = ({ className = "" }: Props) => {
+  return (
+    <div className={className}>
+      <HStack justifyContent={"space-between"} px={5} py={3}>
+        <Circle size="65px" bg="tomato" bgGradient="linear(to-t,gray.800 50%,white 50%)">
+          <Image src={logo} borderRadius={"30px"} boxSize={"60px"}></Image>
+        </Circle>
         <ToggleModeButton />
-    </HStack>
-  </div>;
+      </HStack>
+    </div>
+  );
 };
 
 export default NavBar;

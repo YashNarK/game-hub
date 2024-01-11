71bed02fb538f0a832819bf60826b1b503574b88
YashNarK <narenkrithick@gmail.com>
2024-01-11 10:33:47 +0530

    GameGrid defined responsively. Card building in progress

--- src/components/GameGrid/GameGrid.tsx
+++ src/components/GameGrid/GameGrid.tsx
@@ -1,5 +1,6 @@
-import { Card, CardBody, Heading, Image, Stack, Text } from "@chakra-ui/react";
+import { Card, CardBody, Grid, GridItem, Heading, Image, Stack, Text } from "@chakra-ui/react";
 import useGames from "../../hooks/useGames";
+import StarRatings from "../StarRatings";
 
 const GameGrid = () => {
   const { games, isLoading, httpErrors, setGames, setHttpErrors } = useGames();
@@ -7,19 +8,24 @@ const GameGrid = () => {
   console.log(games[0])
   return (
     <>
-      <ul>
+      <Grid templateColumns={{
+        base:"repeat(1, 1fr)",
+        md: "repeat(3, 1fr)"
+      }} gap={4}>
         {games.map((game) => (
-          <Card maxW={'sm'} key={game.id}>
+          <GridItem key={game.id}>
+            <Card maxW={'sm'} >
             <CardBody>
-              <Image src={game.background_image} alt="Game background Image" borderRadius={'lg'} />
+              <Image src={game.background_image} alt="Game background Image" borderRadius={'lg'} h={200} w={350}/>
               <Stack mt='6' spacing='3'>
                 <Heading size={'md'} >{game.name}</Heading>
-
+                <StarRatings ratings={game.rating} />
               </Stack>
             </CardBody>
           </Card>
+          </GridItem>
         ))}
-      </ul>
+      </Grid>
     </>
   );
 };
---
+++ src/components/StarRatings/StarRatings.tsx
@@ -0,0 +1,31 @@
+import { Box, Center, HStack, Text } from "@chakra-ui/react";
+import { IoIosStar } from "react-icons/io";
+import { IoIosStarHalf } from "react-icons/io";
+
+interface Props {
+  ratings: number;
+}
+
+const generateStars = (numberOfStars: number) => {
+  const absoluteStars = Math.floor(numberOfStars);
+  const stars = [];
+  for (let index = 0; index < absoluteStars; index++) {
+    stars.push(<IoIosStar key={index}/>);
+  }
+  if (numberOfStars - absoluteStars > 0) stars.push(<IoIosStarHalf key={absoluteStars+1}/>);
+
+  return <HStack>{stars}</HStack>;
+};
+
+const StarRatings = ({ ratings }: Props) => {
+  return (
+    <>
+      <HStack justifyContent={'start'} spacing={2}>
+        <Text>{ratings}</Text>
+        {generateStars(ratings)}
+      </HStack>
+    </>
+  );
+};
+
+export default StarRatings;

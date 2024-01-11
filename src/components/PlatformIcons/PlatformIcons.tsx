2c65090b0586834a2fc50c1539ef6af730a5cf02
YashNarK <narenkrithick@gmail.com>
2024-01-12 01:30:41 +0530

    PlatformIcons component code is almost done. need to be tested

--- src/components/PlatformIcons/PlatformIcons.tsx
+++ src/components/PlatformIcons/PlatformIcons.tsx
@@ -9,10 +9,34 @@ import {
 import { MdPhoneIphone } from "react-icons/md";
 import { SiNintendo } from "react-icons/si";
 import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
+import { HStack, Icon } from "@chakra-ui/react";
 
+interface Props {
+  listOfPlatformSlugs: string[];
+}
 
-const PlatformIcons = () => {
-  return <div>PlatformIcons</div>;
+const iconMap: { [key: string]: IconType } = {
+  pc: FaWindows,
+  playstation: FaPlaystation,
+  xbox: FaXbox,
+  nintendo: SiNintendo,
+  mac: FaApple,
+  linux: FaLinux,
+  ios: MdPhoneIphone,
+  web: BsGlobe,
+  android: FaAndroid,
+};
+
+const PlatformIcons = ({listOfPlatformSlugs}:Props) => {
+  return (<>
+  <HStack>
+    {
+        listOfPlatformSlugs.map(slug=><Icon as={iconMap[slug]} />)
+    }
+    
+  </HStack>
+  </>);
 };
 
 export default PlatformIcons;

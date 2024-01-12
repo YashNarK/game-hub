ddce43a9963c28b08824a39c62d6e09648c64136
YashNarK <narenkrithick@gmail.com>
2024-01-12 13:33:26 +0530

    GenreList component created.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 
 function App() {
   return (
@@ -21,7 +22,9 @@ function App() {
           <NavBar />
         </GridItem>
         <Show above="md">
-          <GridItem area={"aside"}>ASIDE</GridItem>
+          <GridItem area={"aside"}>
+            <GenreList />
+          </GridItem>
         </Show>
         <GridItem area={"main"}>
           <GameGrid />

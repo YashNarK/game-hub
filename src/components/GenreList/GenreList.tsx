16559aac0ec52ce618aad52521d394975b932e85
YashNarK <narenkrithick@gmail.com>
2024-01-13 00:34:56 +0530

    Genre list hover highlighted

--- src/components/GenreList/GenreList.tsx
+++ src/components/GenreList/GenreList.tsx
@@ -9,11 +9,13 @@ import {
   Button,
   ListIcon,
   Box,
+  useColorMode,
 } from "@chakra-ui/react";
 import useGenres from "../../hooks/useGenres";
 import optimizeImage from "../../services/image-optimizer";
 import { GiVibratingBall } from "react-icons/gi";
 import { GenreData } from "../../services/genre-service";
+import { useState } from "react";
 
 interface Props {
   onGenreSelect: (selectedGenre: GenreData | null) => void;
@@ -22,14 +24,22 @@ interface Props {
 
 const GenreList = ({ onGenreSelect, selectedGenre }: Props) => {
   const { genres, isLoading, httpErrors } = useGenres();
-  const underLineGenre = () => {
+  const [hoveredGenre, setHoveredGenre] = useState<number>(-1);
+  const { colorMode } = useColorMode();
+  
+  const highlightProps = {
+    color: colorMode === "dark" ? "yellow" : "black",
+    height: colorMode === "dark" ? 0.5 : 2,
+  };
+  
+  const underLineGenre = (color?: string, heightInPixels?: number) => {
     return (
       <Box
         as="span"
         display="block"
-        height="2px"
+        height={heightInPixels ? `${heightInPixels}px` : "2px"}
         width="100px"
-        bg="gray"
+        bg={color ? color : "gray"}
         ml={"40px"}
       />
     );
@@ -53,23 +63,30 @@ const GenreList = ({ onGenreSelect, selectedGenre }: Props) => {
       {isLoading && <Spinner size={"xl"} />}
 
       {!httpErrors && (
-        <List spacing={2} maxH={'15cm'} overflow={'auto'}>
+        <List spacing={2} maxH={"15cm"} overflow={"auto"}>
           <ListItem py={1}>
             <HStack spacing={2}>
               <ListIcon as={GiVibratingBall} boxSize={"32px"} />{" "}
               <Button
+                _hover={{ textDecoration: "none" }}
+                justifyContent={"start"}
+                whiteSpace={"normal"}
+                width={"200px"}
                 fontWeight={!selectedGenre ? "bold" : "normal"}
                 onClick={() => {
                   onGenreSelect(null);
                 }}
                 variant={"link"}
                 fontSize={"lg"}
+                onMouseEnter={() => setHoveredGenre(0)}
+                onMouseLeave={() => setHoveredGenre(-1)}
               >
                 All
               </Button>
             </HStack>
 
-            {!selectedGenre && underLineGenre()}
+            {(!hoveredGenre && underLineGenre(highlightProps.color,highlightProps.height)) ||
+              (!selectedGenre && underLineGenre())}
           </ListItem>
           {genres.map((genre) => (
             <ListItem key={genre.id} py={1}>
@@ -82,6 +99,10 @@ const GenreList = ({ onGenreSelect, selectedGenre }: Props) => {
                   borderRadius={8}
                 />
                 <Button
+                  _hover={{ textDecoration: "none" }}
+                  justifyContent={"start"}
+                  whiteSpace={"normal"}
+                  width={"200px"}
                   fontWeight={
                     selectedGenre?.id === genre.id ? "bold" : "normal"
                   }
@@ -90,11 +111,16 @@ const GenreList = ({ onGenreSelect, selectedGenre }: Props) => {
                   }}
                   variant={"link"}
                   fontSize={"lg"}
+                  onMouseEnter={() => setHoveredGenre(genre.id)}
+                  onMouseLeave={() => setHoveredGenre(-1)}
+                  
                 >
                   {genre.name}
                 </Button>
               </HStack>
-              {selectedGenre?.id === genre.id && underLineGenre()}
+              {(hoveredGenre === genre.id &&
+                underLineGenre(highlightProps.color,highlightProps.height)) ||
+                (selectedGenre?.id === genre.id && underLineGenre())}
             </ListItem>
           ))}
         </List>

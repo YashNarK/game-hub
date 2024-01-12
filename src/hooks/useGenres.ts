c3493d273a977188a51f31579322ecebb102d010
YashNarK <narenkrithick@gmail.com>
2024-01-12 13:50:35 +0530

    useGenres re written using useData generic hook

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,44 +1,16 @@
-import { useEffect, useState } from "react";
 import genreService, {
   GenreData,
-  GenreResponseData,
 } from "../services/genre-service";
-import { AxiosError } from "../services/api-client";
+import useData from "./useData";
 
 const useGenres = () => {
-  const [genres, setGenres] = useState<GenreData[]>([]);
-  const [httpErrors, setHttpErrors] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    let controllerAbort: () => void;
-
-    const fetchGenres = async () => {
-      try {
-        setIsLoading(true);
-        const { resp, cancel } = await genreService.get<GenreResponseData>();
-        controllerAbort = cancel;
-        const genresList = resp.data.results;
-        setGenres(genresList);
-        setHttpErrors("");
-      } catch (error) {
-        const err = error as AxiosError;
-        setHttpErrors(err.message);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchGenres();
-
-    return () => controllerAbort && controllerAbort();
-  }, []);
+  const {data,setData, httpErrors,setHttpErrors,isLoading,setIsLoading} = useData<GenreData>(genreService);
 
   return {
-    genres,
+    genres:data,
     httpErrors,
     isLoading,
-    setGenres,
+    setGenres:setData,
     setHttpErrors,
     setIsLoading,
   };

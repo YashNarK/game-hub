59a082affdac57b7a4456a070b0a52d2e6220df9
YashNarK <narenkrithick@gmail.com>
2024-01-15 00:59:20 +0530

    handle games with no image

--- src/services/image-optimizer.ts
+++ src/services/image-optimizer.ts
@@ -1,5 +1,7 @@
+import noImage from '../assets/noImage.jpg'
+
 const optimizeImage = (imageUrl: string) => {
-  if (!imageUrl) return '';
+  if (!imageUrl) return noImage;
   const target = "media/";
   const index = imageUrl.indexOf(target) + target.length;
   return imageUrl.slice(0, index) + `crop/600/400/` + imageUrl.slice(index);

111c2485a5c25a3eea551c65045bfb0f2bb2b95b
YashNarK <narenkrithick@gmail.com>
2024-02-13 22:28:30 +0530

    installed  @tanstack/router-vite-plugin  and @tanstack/react-router, and configured it in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/game-hub",
-  plugins: [react()],
+  plugins: [react(), TanStackRouterVite()],
 });

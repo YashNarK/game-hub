660a92928962a68e3ba9f76c0d6071901eada798
YashNarK <narenkrithick@gmail.com>
2024-02-16 01:06:02 +0530

    a new page GameDetailsPage is now almost done

--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,8 +1,33 @@
 import { useParams } from "react-router-dom";
+import useGameDetails from "../hooks/useGameDetails";
+import { Box, Heading, Spinner, Text } from "@chakra-ui/react";
 
 const GameDetailsPage = () => {
-    const {gamesId} = useParams()
-  return <div>GameDetailsPage: {gamesId}</div>;
+  const { slug } = useParams();
+  const { data: game, error, isLoading } = useGameDetails(slug!);
+
+  if (isLoading)
+    return (
+      <Box textAlign={"center"} px={4}>
+        <Spinner size={"xl"} />{" "}
+        <Heading>The game details are being fetched</Heading>
+      </Box>
+    );
+
+  if (error || !game)
+    return (
+      <Box textAlign={"center"} px={4}>
+        <Text>The game was not found. </Text>
+        <Heading as="h3">Error: {error?.message}</Heading>
+      </Box>
+    );
+
+  return (
+    <Box py={3} px={4}>
+      <Heading>{game?.name}</Heading>
+      <Text>{game?.description_raw}</Text>
+    </Box>
+  );
 };
 
 export default GameDetailsPage;
--- src/routes.tsx
+++ src/routes.tsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: "games/:gamesId", element: <GameDetailsPage /> },
+      { path: "games/:slug", element: <GameDetailsPage /> },
     ],
   },
 ]);

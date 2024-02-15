acab4915123032c774b0348e4873c87eb3425ab1
YashNarK <narenkrithick@gmail.com>
2024-02-16 02:14:25 +0530

    logic adjustment for expandable text component

--- src/components/ExpandableText/ExpandableText.tsx
+++ src/components/ExpandableText/ExpandableText.tsx
@@ -11,31 +11,36 @@ const ExpandableText = ({ children }: Props) => {
   const { highlightColor } = useColorModes();
   const [isMaxLine, setIsMaxLine] = useState(true);
 
-  const finalText = isMaxLine ? children.substring(0, 650) + "..." : children;
+  const finalText =
+    isMaxLine && children.length >= 650
+      ? children.substring(0, 650) + "..."
+      : children;
   return (
     <>
       <Text>{finalText}</Text>
-      {children.length>650 && <Box textAlign={"center"} my={3}>
-        {" "}
-        <Button
-          variant={"outline"}
-          onClick={() => {
-            setIsMaxLine(!isMaxLine);
-          }}
-          colorScheme={highlightColor}
-          fontSize={"bolder"}
-        >
-          {isMaxLine ? (
-            <>
-              <FaAngleDoubleDown /> Read More...
-            </>
-          ) : (
-            <>
-              <FaAngleDoubleUp /> Show Less...
-            </>
-          )}
-        </Button>
-      </Box>}
+      {children.length >= 650 && (
+        <Box textAlign={"center"} my={3}>
+          {" "}
+          <Button
+            variant={"outline"}
+            onClick={() => {
+              setIsMaxLine(!isMaxLine);
+            }}
+            colorScheme={highlightColor}
+            fontSize={"bolder"}
+          >
+            {isMaxLine ? (
+              <>
+                <FaAngleDoubleDown /> Read More...
+              </>
+            ) : (
+              <>
+                <FaAngleDoubleUp /> Show Less...
+              </>
+            )}
+          </Button>
+        </Box>
+      )}
     </>
   );
 };

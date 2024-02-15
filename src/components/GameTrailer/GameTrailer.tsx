ad26576cdd733f413490b3dcb22bbdd65442cf8f
YashNarK <narenkrithick@gmail.com>
2024-02-16 02:59:57 +0530

    GameTrailer Component created and added to GameDetailsPage

---
+++ src/components/GameTrailer/GameTrailer.tsx
@@ -0,0 +1,31 @@
+import { Box, Heading, Spinner } from "@chakra-ui/react";
+import useGameTrailers from "../../hooks/useGameTrailers";
+
+interface Props {
+  gameId: number;
+}
+const GameTrailer = ({ gameId }: Props) => {
+  const { data: Trailer, error, isLoading } = useGameTrailers(gameId);
+  if (isLoading)
+    return (
+      <Box>
+        <Spinner size={"xl"} /> <Heading>Trailer is Loading...</Heading>
+      </Box>
+    );
+
+  if (error) return <Heading>An error occured: {error.message}</Heading>;
+
+  const first = Trailer?.results[0];
+  return first ? (
+    <Box display={"flex"} justifyContent={"center"}>
+      <video
+        width="100%"
+        src={first.data[480]}
+        poster={first.preview}
+        controls
+      />
+    </Box>
+  ) : null;
+};
+
+export default GameTrailer;

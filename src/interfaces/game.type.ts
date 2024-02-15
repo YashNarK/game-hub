19130e39d436aca79fa4724748d78ed0d20c16b0
YashNarK <narenkrithick@gmail.com>
2024-02-16 00:25:12 +0530

    now slug is added as a property to GameData

--- src/interfaces/game.type.ts
+++ src/interfaces/game.type.ts
@@ -2,25 +2,25 @@ import { GenreData } from "./genre.type";
 import { ParentPlatformData, PlatformData } from "./platform.type";
 
 export interface GameData {
-    id: number;
-    name: string;
-    description: string;
-    background_image: string;
-    website: string;
-    rating: number;
-    parent_platforms: PlatformData[];
-    metacritic:number;
-    genres:GenreData[]
-  }
+  id: number;
+  name: string;
+  description: string;
+  background_image: string;
+  website: string;
+  rating: number;
+  parent_platforms: PlatformData[];
+  metacritic: number;
+  genres: GenreData[];
+  slug: string;
+}
 
-  export interface GameQuery {
-    genre?: GenreData | null | undefined;
-    platform?: ParentPlatformData | null | undefined;
-    ordering: string | null | undefined;
-    isAscending: boolean | undefined;
-    search: string | undefined;
-    platformName: string | undefined;
-    genreName: string | undefined;
-    page: number;
-  }
-  
+export interface GameQuery {
+  genre?: GenreData | null | undefined;
+  platform?: ParentPlatformData | null | undefined;
+  ordering: string | null | undefined;
+  isAscending: boolean | undefined;
+  search: string | undefined;
+  platformName: string | undefined;
+  genreName: string | undefined;
+  page: number;
+}
